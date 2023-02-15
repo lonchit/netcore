51967ecf43a4a01505064f80bdf00eae33675670
lonchit <zhemchugov97@gmail.com>
2023-02-15 19:12:27 +0300

    re

---
+++ src/Nucleus.Web.Vue/src/admin/admin-layout.ts
@@ -0,0 +1,11 @@
+﻿import {defineComponent} from "vue";
+import AsideMenu from "@/admin/components/menu/aside-menu/aside-menu.vue";
+import TopMenu from "@/admin/components/menu/top-menu/top-menu.vue";
+
+export default  defineComponent({
+    name:"AdminLayoutComponent",
+    components: {
+        AsideMenu,
+        TopMenu
+    },
+})
---
+++ src/Nucleus.Web.Vue/src/admin/admin-layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200">
+      <AsideMenu/>
+      
+      <div class="flex-1 flex flex-col overflow-hidden">
+
+        <TopMenu/>
+        
+        <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+          <div class="container mx-auto px-4 py-4">
+            <router-view/>
+          </div>
+        </main>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script src="./admin-layout.ts"></script>
